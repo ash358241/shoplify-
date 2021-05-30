9b3a6ec61953d01de59ba72648a358e239653994
Towhidul1999 <towhidulsislam@gmail.com>
2021-05-30 17:41:04 +0600

    added-more-feature

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Header from './containers/Header';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ProductListing from './containers/ProductListing';
-import ProductDetail from './containers/ProductComponent';
+import ProductDetail from './containers/ProductDetail';
 
 function App() {
   return (
